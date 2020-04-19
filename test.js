8cc4646ef73a0af27a40edb72c4e0ca382ba7472
Jannis R <mail@jannisr.de>
2020-04-19 15:16:36 +0200

    WIP

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 'use strict'
 
-const assert = require('assert')
-const todo = require('.')
+const a = require('assert')
+const sql = require('./lib/sql')
 
+a.strictEqual(sql `\
+INSERT INTO foo (
+	bar,
+	baz,
+	qux
+) VALUES (
+	${1},
+	${'hey there'},
+	${null}
+)`, `\
+INSERT INTO foo (
+	bar,
+	baz,
+	qux
+) VALUES (
+	1,
+	'hey there',
+	NULL
+)`)
 // todo
