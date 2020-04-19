e59a8795730472e8ce17cb617f0c29b8f9d8a2d8
Jannis R <mail@jannisr.de>
2020-04-19 18:36:05 +0200

    WIP

--- cli.js
+++ cli.js
@@ -30,11 +30,33 @@ if (argv.version || argv.v) {
 const {basename, extname} = require('path')
 const readCsv = require('gtfs-utils/read-csv')
 const {Transform} = require('stream')
+const stdout = require('stdout-stream')
 const {format, schemas} = require('.')
-const sql = require('./lib/sql')
+const converter = require('./lib/converter')
+
+const files = argv._
+
+const pump = (src, through, dest) => {
+	return new Promise((resolve, reject) => {
+		src.once('error', reject)
+		through.once('error', reject)
+		dest.once('error', reject)
+		through.once('end', () => {
+			src.removeListener('error', reject)
+			through.removeListener('error', reject)
+			dest.removeListener('error', reject)
+			resolve()
+		})
+		src.pipe(through).pipe(dest)
+	})
+}
 
 ;(async () => {
-	const files = argv._
+	process.stdout.write(`\
+\\set ON_ERROR_STOP on;
+CREATE EXTENSION IF NOT EXISTS postgis;
+\n`)
+
 	for (const file of files) {
 		const name = basename(file, extname(file))
 		process.stdout.write(`-- ${name}\n-----------------\n\n`)
@@ -46,45 +68,11 @@ const sql = require('./lib/sql')
 		process.stdout.write(schema + '\n\n')
 
 		const formatter = format[name]
-		let first = true
-		function write (row, _, cb) {
-			if (first) {
-				first = false
-				this.push(formatter(sql, row, true))
-			} else {
-				this.push(formatter(sql, row, false))
-			}
-			cb()
-		}
-		function writev (chunks, _, cb) {
-			for (let i = 0; i < chunks.length; i++) {
-				const row = chunks[i].chunk
-				if (first) {
-					first = false
-					this.push(formatter(sql, row, true))
-				} else {
-					this.push(formatter(sql, row, false))
-				}
-			}
-			cb()
-		}
-		const convert = new Transform({objectMode: true, write}) // todo
-
-		await new Promise((resolve, reject) => {
-			const src = readCsv(file)
-			const dest = process.stdout
-			const done = () => {
-				src.removeListener('error', reject)
-				convert.removeListener('error', reject)
-				dest.removeListener('error', reject)
-				resolve()
-			}
-			src.once('error', reject)
-			convert.once('error', reject)
-			dest.once('error', reject)
-			convert.once('end', done)
-			src.pipe(convert).pipe(dest)
-		})
+		await pump(
+			readCsv(file),
+			converter(formatter),
+			stdout,
+		)
 	}
 })()
 .catch((err) => {
