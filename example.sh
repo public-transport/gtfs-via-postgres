560744ecfb2b35feb0c6da9230f94f3042a50dc5
Jannis R <mail@jannisr.de>
2021-04-21 14:33:57 +0200

    example.sh: how to query connections/arrivals_departures 📝; 2.8.3

--- example.sh
+++ example.sh
@@ -3,5 +3,33 @@
 set -e
 set -o pipefail
 
-echo 'importing to PostgreSQL'
-./cli.js node_modules/sample-gtfs-feed/gtfs/*.txt | psql -b
+2>&1 echo "importing into PostgreSQL:"
+./cli.js --ignore-unsupported --require-dependencies --trips-without-shape-id --silent \
+	node_modules/sample-gtfs-feed/gtfs/*.txt | psql -b
+
+2>&1 echo "\nfetching a connection during DST switch:"
+psql -c "$(cat <<- EOM
+	SELECT
+		trip_id, route_id,
+		from_stop_id, t_departure,
+		stop_sequence,
+		to_stop_id, t_arrival
+	FROM connections
+	WHERE trip_id = 'during-dst-1'
+	AND t_departure > '2019-03-31T01:55+01' AND t_departure < '2019-03-31T03:00+02'
+	-- AND route_id = 'D'
+	-- AND from_stop_id = 'airport'
+EOM)"
+
+2>&1 echo "\nfetching the departure at the same time:"
+psql -c "$(cat <<- EOM
+	SELECT
+		trip_id, route_id,
+		stop_id, t_departure,
+		stop_sequence
+	FROM arrivals_departures
+	WHERE trip_id = 'during-dst-1'
+	AND t_departure > '2019-03-31T01:55+01' AND t_departure < '2019-03-31T03:00+02'
+	-- AND route_id = 'D'
+	-- AND stop_id = 'airport'
+EOM)"
