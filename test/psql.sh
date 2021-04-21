adc3892c156ae48e030576b8fab6b53b8db68108
Jannis R <mail@jannisr.de>
2021-04-21 14:16:02 +0200

    connections: handle missing to_stations row 🐛

--- test/psql.sh
+++ test/psql.sh
@@ -37,4 +37,18 @@ if [[ "$arr2" != "1553994180" ]]; then
 	exit 1
 fi
 
+connection_during_dst=$(cat << EOF
+	SELECT
+		stop_sequence,
+		extract(epoch from t_departure) as dep
+	FROM connections
+	WHERE trip_id = 'during-dst-1'
+	AND t_departure = '2019-03-31T01:58+01'
+EOF)
+dst1=$(psql --csv -t -c "$connection_during_dst" | head -n 1)
+if [[ "$dst1" != "0,1553993880" ]]; then
+	echo "invalid/missing DST t_departure: $dst1" 1>&2
+	exit 1
+fi
+
 echo ✔︎
