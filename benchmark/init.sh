b2cc9e230340db8e0fa0eb5f20cebde5cbe7a7c0
Jannis R <mail@jannisr.de>
2023-07-12 14:47:43 +0200

    add benchmark init script ⚡️

---
+++ benchmark/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+cd "$(dirname "$0")"
+set -x
+
+wget --compression auto -r --no-parent --no-directories -R .csv.gz -P ../vbb-2022-07-01.gtfs -N 'https://vbb-gtfs.jannisr.de/2022-07-01/'
+ls -lh ../vbb-2022-07-01.gtfs
+
+env | grep '^PG' || true
+
+../cli.js -d \
+	--stops-location-index --stats-by-route-date=view \
+	../vbb-2022-07-01.gtfs/*.csv | sponge | psql -b
