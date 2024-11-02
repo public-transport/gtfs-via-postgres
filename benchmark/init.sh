fa65b0ee71940d530b67e8df9eb9d09e0c7e2c02
Jannis R <mail@jannisr.de>
2024-11-02 16:43:20 +0100

    benchmark: also run on gtfstidy-ed GTFS ⚡️

--- benchmark/init.sh
+++ benchmark/init.sh
@@ -18,3 +18,18 @@ export PGDATABASE=benchmark_raw
 	../vbb-2022-07-01.gtfs/*.csv | sponge | psql -b
 
 ./run.sh
+
+# The VBB 2022-07-01 GTFS feed doesn't contain any frequencies rows. In order to benchmark the frequencies implementations, we use `--minimize-stoptimes` to generate an equivalent feed that includes frequencies.
+gtfstidy --show-warnings \
+	--minimize-stoptimes \
+	-o ../vbb-2022-07-01.tidied.gtfs ../vbb-2022-07-01.gtfs
+ls -lh ../vbb-2022-07-01.tidied.gtfs
+
+psql -c 'CREATE DATABASE benchmark_tidied'
+export PGDATABASE=benchmark_tidied
+
+../cli.js -d \
+	--stops-location-index --stats-by-route-date=view \
+	../vbb-2022-07-01.tidied.gtfs/*.txt | sponge | psql -b
+
+./run.sh
