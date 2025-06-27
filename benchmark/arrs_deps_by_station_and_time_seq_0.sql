44e78f490eda03ee1b4c93b9315ff8575e25dd99
Jannis R <mail@jannisr.de>
2025-06-27 15:30:01 +0200

    benchmarks & readme: update to 2025-05-21 VBB GTFS dataset 📝⚡️

--- benchmark/arrs_deps_by_station_and_time_seq_0.sql
+++ benchmark/arrs_deps_by_station_and_time_seq_0.sql
@@ -1,7 +1,7 @@
 SELECT *
 FROM arrivals_departures
 WHERE station_id = 'de:11000:900100001' -- S+U Friedrichstr. (Berlin)
-AND t_departure >= '2022-08-09T07:10:00+02' AND t_departure <= '2022-08-09T07:30:00+02'
-AND date >= dates_filter_min('2022-08-09T07:10:00+02')
-AND date <= dates_filter_max('2022-08-09T07:30:00+02')
+AND t_departure >= '2025-05-27T07:10:00+02' AND t_departure <= '2025-05-27T07:30:00+02'
+AND date >= dates_filter_min('2025-05-27T07:10:00+02')
+AND date <= dates_filter_max('2025-05-27T07:30:00+02')
 AND stop_sequence = 0
