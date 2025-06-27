d66287dbc12278fa2fb7e340e0238be17e275962
Jannis R <mail@jannisr.de>
2025-06-27 13:44:47 +0200

    DuckDB rewrite: use ISO 8601 timestamps including seconds 📝

--- benchmark/arrs_deps_by_station_and_time_seq_0.sql
+++ benchmark/arrs_deps_by_station_and_time_seq_0.sql
@@ -1,7 +1,7 @@
 SELECT *
 FROM arrivals_departures
 WHERE station_id = 'de:11000:900100001' -- S+U Friedrichstr. (Berlin)
-AND t_departure >= '2022-08-09T07:10+02' AND t_departure <= '2022-08-09T07:30+02'
-AND date >= dates_filter_min('2022-08-09T07:10+02')
-AND date <= dates_filter_max('2022-08-09T07:30+02')
+AND t_departure >= '2022-08-09T07:10:00+02' AND t_departure <= '2022-08-09T07:30:00+02'
+AND date >= dates_filter_min('2022-08-09T07:10:00+02')
+AND date <= dates_filter_max('2022-08-09T07:30:00+02')
 AND stop_sequence = 0
