e8784aa49490a37835b132a06e94b03a9e02cfb6
Jannis R <mail@jannisr.de>
2025-06-27 15:30:01 +0200

    benchmarks & readme: update to 2025-05-21 VBB GTFS dataset 📝⚡️

--- benchmark/connections_by_time_manual.sql
+++ benchmark/connections_by_time_manual.sql
@@ -1,7 +1,6 @@
 SELECT *
 FROM connections
-WHERE t_departure >= '2022-08-09T07:10:00+02' AND t_departure <= '2022-08-09T07:30:00+02'
-AND date >= '2022-08-08'
-AND date <= '2022-08-09'
+WHERE t_departure >= '2025-05-27T07:10:00+02' AND t_departure <= '2025-05-27T07:30:00+02'
+AND date >= '2025-05-25' AND date <= '2025-05-27'
 ORDER BY t_departure
 LIMIT 100
--- benchmark/init.sh
+++ benchmark/init.sh
@@ -5,12 +5,12 @@ set -o pipefail
 cd "$(dirname "$0")"
 set -x
 
-wget --compression auto -r --no-parent --no-directories -R .csv.gz -P ../vbb-2022-07-01.gtfs -N 'https://vbb-gtfs.jannisr.de/2022-07-01/'
-ls -lh ../vbb-2022-07-01.gtfs
+wget --compression auto -r --no-parent --no-directories -R .csv.gz,.csv.br -P ../vbb-2025-05-21.gtfs -N 'https://vbb-gtfs.jannisr.de/2025-05-21/'
+ls -lh ../vbb-2025-05-21.gtfs
 
 env | grep '^PG' || true
 
 ../cli.js -d \
 	--stops-location-index --stats-by-route-date=view \
-	vbb-2022-07-01.gtfs.duckdb \
-	../vbb-2022-07-01.gtfs/*.csv
+	vbb-2025-05-21.gtfs.duckdb \
+	../vbb-2025-05-21.gtfs/*.csv
