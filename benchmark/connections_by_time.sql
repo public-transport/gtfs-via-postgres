e4b7d4ac7a87bef26c0f06fef543e0869b2d94f8
Jannis R <mail@jannisr.de>
2025-06-27 13:44:47 +0200

    DuckDB rewrite: use ISO 8601 timestamps including seconds 📝

--- benchmark/connections_by_time.sql
+++ benchmark/connections_by_time.sql
@@ -1,7 +1,7 @@
 SELECT *
 FROM connections
-WHERE t_departure >= '2022-08-09T07:10+02' AND t_departure <= '2022-08-09T07:30+02'
-AND date >= dates_filter_min('2022-08-09T07:10+02'::timestamp with time zone)
-AND date <= dates_filter_max('2022-08-09T07:30+02'::timestamp with time zone)
+WHERE t_departure >= '2022-08-09T07:10:00+02' AND t_departure <= '2022-08-09T07:30:00+02'
+AND date >= dates_filter_min('2022-08-09T07:10:00+02'::timestamp with time zone)
+AND date <= dates_filter_max('2022-08-09T07:30:00+02'::timestamp with time zone)
 ORDER BY t_departure
 LIMIT 100
